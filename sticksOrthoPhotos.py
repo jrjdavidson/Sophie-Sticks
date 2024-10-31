120fda79d22e78be405e6c665cb772dbaffcf5ed
KengelUC <Kamen.engel@canterbury.ac.nz>
2024-11-01 11:32:17 +1300

    Initial

---
+++ sticksOrthoPhotos.py
@@ -0,0 +1,94 @@
+import Metashape
+import os
+
+def find_files(folder, types):
+    return [entry.path for entry in os.scandir(folder) if (entry.is_file() and os.path.splitext(entry.name)[1].upper().lstrip('.') in types)]
+
+def setUpPhotos(image_folder, project_path, types=['JPG']):
+    photos = find_files(image_folder, types)
+
+    doc = Metashape.Document()
+    doc.save(project_path)
+
+    
+    chunk = doc.addChunk()
+    chunk.addPhotos(photos)
+    print(str(len(chunk.cameras)) + " images loaded")
+
+    doc.save()
+    return doc
+
+def process_folders(root_folder):
+    # for every folder(entry) in the root
+    docs=[]
+    for entry in os.scandir(root_folder):
+        #if the entry is a directory
+        if entry.is_dir():
+            #define the folder name
+            folder_name = os.path.basename(entry.path.rstrip(os.sep))
+            psx_name = folder_name + '.psx'
+            psx_path = os.path.join(entry.path, psx_name)
+            #if the psx file doesn't exist then create one
+            if not os.path.exists(psx_path):
+                print(f'Processing folder: {entry.path}')
+                doc = setUpPhotos(entry.path, psx_path)
+                # Add marker detection and assignment if needed
+                docs.append(doc)
+                #skips setting up folder
+            else:
+                # doc= Metashape.Document()
+                # doc.open(psx_path,read_only=False)
+                # docs.append(doc)
+                print(f'Skipping folder {entry.path}, Metashape file already exists.')
+    return docs
+
+def process_doc(doc):
+    doc.save()
+    file_path = doc.path
+    name = os.path.splitext(file_path)[0]
+    chunk = doc.chunks[0]
+    chunk.detectMarkers()
+    markerLocation={
+        'target 1': Metashape.Vector((0.32,0,0)),
+        'target 2': Metashape.Vector((0.52,0,0)),
+        'target 3': Metashape.Vector((0.718,0,0))
+    }
+    for marker in chunk.markers:
+        print(f'Maker label is {marker.label}')
+        for key in markerLocation.keys():
+            print(f'Key is: {key}')
+            if marker.label in markerLocation and marker.label == key:
+                print(f'{marker.label} is in markerLocation dictonary and the key and marker label are equal.')
+                marker.reference.location= markerLocation[key]
+                
+    doc.save()
+
+    chunk.matchPhotos()
+    chunk.alignCameras()
+    doc.save()
+
+    chunk.buildDepthMaps()
+    chunk.buildModel(source_data=Metashape.DepthMapsData)
+    chunk.buildOrthomosaic()
+    doc.save()
+
+    try:
+        chunk.exportRaster(path=name + '.tif')
+    except Exception as error:
+        print(f'An error occured: {error}')
+
+if __name__ == '__main__':
+    root_folder = '\\\\file\\Shared\\SEESPhotoDatabase\\Active Work\\Kamen Engel\\For Sophie Newsham\\Working Files'
+    docs =  process_folders(root_folder)
+    for doc in docs:
+        print(f'Processing {doc.path}')
+        process_doc(doc)
+        # Close the document explicitly
+        del doc
+          
+        
+    
+
+
+
+
